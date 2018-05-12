36d073825b6ef327a7d0240ce3ccc4d725989ca0
tchir <samuel.ritchie@ensae.fr>
2018-05-13 00:54:23 +0200

    Add bin folder

---
+++ bin/annotate.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import click
+import yaml
+import logging
+import logging.config
+from scrapping import annotate
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+def setup_logging(default_path='logging.yaml', default_level=logging.INFO, env_key='LOG_CFG'):
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@click.command(name='annotate-links')
+@click.option('--links', default='data/triples.txt')
+def annotate_links(links):
+    output = annotate(links)
+    output.to_csv('data/metadata_dataframe.csv', index=False)
+
+
+cli.add_command(annotate_links)
+
+if __name__ == '__main__':
+    setup_logging()
+    cli()
