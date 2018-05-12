421c2dcefc4b66e3296a296163564315cb2df568
tchir <samuel.ritchie@ensae.fr>
2018-05-13 00:10:41 +0200

    Initial commit

---
+++ scrapping.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import urllib
+import bs4
+import logging.config
+logger = logging.getLogger(__name__)
+
+
+def scrapping_jazz_metadata(url):
+    print('fetching metadata for url%s' % url)
+    try:
+        request_text = urllib.request.urlopen(url).read()
+
+        page = bs4.BeautifulSoup(request_text, "lxml")
+
+        name = page.find('title').getText().split('About: ')[1]
+
+        if page.find('span', {'property': 'dbo:background'}) is not None:
+            background = page.find('span', {'property': 'dbo:background'}).getText()
+        else:
+            background = None
+
+        if page.find('span', {'property': 'dbo:birthDate'}) is not None:
+            birth_year = page.find('span', {'property': 'dbo:birthDate'}).getText()[0:4]
+        else:
+            birth_year = None
+
+        if page.findAll('a', {'rel': 'dbo:birthPlace'}):
+            birth_place = [p.getText().split('dbr:')[1] for p in page.findAll('a', {'rel': 'dbo:birthPlace'})]
+        else:
+            birth_place = None
+
+        if page.find('span', {'property': 'dbo:deathDate'}) is not None:
+            death_year = page.find('span', {'property': 'dbo:deathDate'}).getText()[0:4]
+        else:
+            death_year = None
+
+        if page.find('a', {'rel': 'dbo:deathPlace'}) is not None:
+            death_place = page.find('a', {'rel': 'dbo:deathPlace'}).getText().split('dbr:')[1]
+        else:
+            death_place = None
+
+        if page.findAll('a', {'rel': 'dbo:genre'}):
+            genre = [p.getText().split('dbr:')[1] for p in page.findAll('a', {'rel': 'dbo:genre'})]
+        else:
+            genre = None
+
+        if page.findAll('a', {'rel': 'dbo:instrument'}):
+            instrument = [p.getText().split('dbr:')[1] for p in page.findAll('a', {'rel': 'dbo:instrument'})]
+        elif page.findAll('span', {'property': 'dbp:instrument'}):
+            instrument = [p.getText() for p in page.findAll('span', {'property': 'dbp:instrument'})]
+        elif page.findAll('a', {'rel': 'ns1:hypernym'}):
+            instrument = [p.getText().split('dbr:')[1] for p in page.findAll('a', {'rel': 'ns1:hypernym'})]
+        else:
+            instrument = None
+
+        if page.findAll('a', {'rel': 'dbo:associatedMusicalArtist'}):
+            associated_artists = [p.getText().split('dbr:')[1] for p in
+                                  page.findAll('a', {'rel': 'dbo:associatedMusicalArtist'})]
+        else:
+            associated_artists = None
+
+        if page.findAll('a', {'rel': 'dbo:associatedBand'}):
+            associated_bands = [p.getText().split('dbr:')[1] for p in page.findAll('a', {'rel': 'dbo:associatedBand'})]
+        else:
+            associated_bands = None
+
+        result = {'Name': name, 'Background': background, 'Birth Year': birth_year, 'Birth Place': birth_place,
+                  'Death Year': death_year, 'Death Place': death_place, 'Genre': genre, 'Instruments': instrument
+            , 'Associated Artists': associated_artists, 'Associated Bands': associated_bands}
+
+    except:
+        result = {}
+
+    return result
+
+
+def annotate(csv="data/triples.txt"):
+    data = pd.read_csv(csv, sep=" ", header=None, names=['Artiste1', 'Lien', 'Artiste2', '.'])
+    unic_jazzmen = pd.DataFrame(data.Artiste1.append(data.Artiste2).drop_duplicates(), columns=['artist_url'])
+    unic_jazzmen['metadata'] = unic_jazzmen.artist_url.apply(scrapping_jazz_metadata)
+    unic_jazzmen_dataframe = pd.concat([unic_jazzmen.artist_url,unic_jazzmen.metadata.apply(pd.Series)],axis=1).reset_index(drop=True)
+    unic_jazzmen_dataframe.to_csv('data/metadata_dataframe.csv', index=False)
